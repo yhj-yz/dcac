728d4ec4155e18aad336c58b6d9bef1674b2dd6e
itshunn <itshunn@163.com>
2020-02-25 13:54:15 +0800

    结构修改

--- technically/common/src/main/java/com/huatusoft/dcac/common/bo/CommonAttributes.java
+++ technically/common/src/main/java/com/huatusoft/dcac/common/bo/CommonAttributes.java
@@ -12,16 +12,16 @@ public final class CommonAttributes {
 	public static final String[] DATE_PATTERNS = new String[] { "yyyy", "yyyy-MM", "yyyyMM", "yyyy/MM", "yyyy-MM-dd", "yyyyMMdd", "yyyy/MM/dd", "yyyy-MM-dd HH:mm:ss", "yyyyMMddHHmmss", "yyyy/MM/dd HH:mm:ss" };
 
 	/**shopxx.xml文件路径 */
-	public static final String ELECTRONICTAG__XML_PATH = "/electronicTag.xml";
+	public static final String ELECTRONICTAG__XML_PATH = "/dcac.xml";
 
 	/** dsmxx.properties文件路径 */
-	public static final String ELECTRONICTAG__PROPERTIES_PATH = "/electronicTag.properties";
+	public static final String ELECTRONICTAG__PROPERTIES_PATH = "/dcac.properties";
 
 	/** dsmxx-db.properties文件路径 */
-	public static final String ELECTRONICTAG_DB_PROPERTIES_PATH = "/electronicTag-db.properties";
+	public static final String ELECTRONICTAG_DB_PROPERTIES_PATH = "/dcac-db.properties";
 
 	/** dsmDB.sql文件路径 */
-	public static final String ELECTRONICTAG_DB_File_PATH = "/init/initElectronicTag.sql";
+	public static final String ELECTRONICTAG_DB_File_PATH = "/init/initDcac.sql";
 	
 	public static final String MYSQL_DB_URL = "jdbc:mysql://{0}:{1}/{2}?useUnicode=true&characterEncoding=UTF-8";
 
